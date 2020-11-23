f1eaefaf06aa603ea91be387c9d84ce715882455
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-11-23 20:29:04 +0100

    exercise_4.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'12
+  get 'welcome/index'
+  # tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
+
+  root 'welcome#index'
+  # tells Rails to map requests to the root of the application to the welcome controller's index action
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
