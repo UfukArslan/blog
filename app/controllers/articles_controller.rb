70cc297366ffc400b715869fc64ee0ae772f927c
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-11-27 22:32:18 +0100

    Showing Articles 5.7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,26 @@
 class ArticlesController < ApplicationController
+
     def new
     end
+
+    def create 
+        # render plain: params[:article].inspect
+        # The render method here is taking a very simple hash with a key of :plain 
+        # and value of params[:article].inspect. The params method is the object which 
+        # represents the parameters (or fields) coming in from the form. The params method 
+        # returns an ActionController::Parameters object, which allows you to access the 
+        # keys of the hash using either strings or symbols. In this situation, the only 
+        # parameters that matter are the ones from the form.
+
+        @article = Article.new(article_params)
+ 
+        @article.save
+        redirect_to @article
+    end
+
+    private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
 end
---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+    # Active Record is smart enough to automatically map column names to model attributes, 
+    # which means you don't have to declare attributes inside Rails models, as that will 
+    # be done automatically by Active Record.
+end
