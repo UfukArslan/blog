d2bfe2f0868f8839f57122f2231ea634d2765e87
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-11-28 22:59:42 +0100

    Adding Some Validation 5.10

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  
   get 'welcome/index'
   # tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
-
-  resources :articles
-
+  
+  
   root 'welcome#index'
   # tells Rails to map requests to the root of the application to the welcome controller's index action
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :articles
 end
